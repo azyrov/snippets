466d15d2f8ac9d559792ff55f8525db19ea17daf
azyrov <for28@yahoo.com>
2026-07-18 21:26:12 +0000

    Add removing an item from an array

--- 127.0.0.1:3000/index.js
+++ 127.0.0.1:3000/index.js
@@ -1,8 +1,8 @@
-let array = [1, [2, 3], 4, 5]
+let array = [1, 2, 3, 4, 5]
 
 let [one, two, three, four, five] = array
 
-console.log(one + two);
+console.log(one);
 console.log(two);
 console.log(three);
 console.log(four);
