3f0a12c36b74a123996a6157c814394f27511cc3
azyrov <for28@yahoo.com>
2026-08-02 14:06:35 +0000

    Add copy method

--- events_clipboard/index.js
+++ events_clipboard/index.js
@@ -2,4 +2,20 @@ let div = document.querySelector("#div");
 
 div.textContent = "hello";
 
-console.log(div.childNodes)
+// // add event for when a text is copied
+// div.addEventListener("copy", function logMessage(e) {
+//     console.error("Don't copy! Text is poisonous!")
+// })
+
+// // make div editable so the text can be cut and pasted into
+// div.contentEditable = true; 
+
+// // add event for when a text is cut
+// div.addEventListener("cut", function logMessage(e) {
+//     console.error("Don't cut! Text is poisonous!")
+// })
+
+// // add event for when a text is pasted
+// div.addEventListener("paste", function logMessage(e) {
+//     console.error("Don't paste! Text is poisonous!")
+// })
