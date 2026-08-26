3ff74d97aba7523a733e648503afd84cae202e83
azyrov <for28@yahoo.com>
2026-08-26 01:27:24 +0000

    Add objects practice files

---
+++ objects/objects.js
@@ -0,0 +1,15 @@
+// creating an obj
+let obj = {};
+console.log(obj)
+
+// adding new props
+obj["name"] = "Tony Stark";
+console.log(obj); // {name: 'Tony Stark'}
+
+// updating props (1)
+obj["name"] = "Doctor Doom";
+console.log(obj); // {name: 'Doctor Doom'}
+
+// updating props (2)
+obj.name = "Tonie Starke";
+console.log(obj); // console.log(obj)
