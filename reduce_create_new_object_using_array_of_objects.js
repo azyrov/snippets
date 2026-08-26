63ea0b4a2fcbe5aa28d197d70450e6abc092423f
azyrov <for28@yahoo.com>
2026-08-26 14:01:13 +0000

    Add comments showing difference betwwen arrays and objects

--- reduce_create_new_object_using_array_of_objects.js
+++ reduce_create_new_object_using_array_of_objects.js
@@ -5,11 +5,29 @@
   {id: 4, item: "Black Widow's Gauntlets", price: 100, quantity: 150},
   {id: 5, item: "Hawkeye's Bow", price: 300, quantity: 0}
  ]
+
+ /* ARRAY vs OBJECT */
+ // array - ordered (has index numbers)
+ // object - unordered (has no index numbers)
+
+ /// index numbers allow us to use iterative method items
+ 
+ // array - can use MOST iterative methods
+ /// this is because most iterative methods use indexed to track and the items between iterations
+ // object - can only use "for...in" loop
+ /// the for..in loop first acquires the keys of the object
+ /// then the for...in loop goes through the prop, targeting the key of the prop
+ /// check how_loops_work.js for more info 
+ /*******************/
  
  function discountInventory(arr) {
 
     const discount = 0.10;
 
+
+    // i added the inex in the cllback so i could simulate the ordered index numbers for the array produced by the map
+    // that said, an object has no index numbers because they are not ordered
+    // i could have used totalItems[currentItem.item] and it still would have worked
     const newInventory = arr.reduce(((totalItems, currentItem, index) => {
         totalItems[index] = {
             id: currentItem.id,
