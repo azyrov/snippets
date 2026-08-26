b05866c4e4c3776d99e163167ca5e2a4e76f32d7
azyrov <for28@yahoo.com>
2026-08-26 02:37:59 +0000

    Add how to create a map from an object list in an array

---
+++ map_create_new_array_using_array_of_objects.js
@@ -0,0 +1,41 @@
+ const inventory = [
+  {id: 1, item: "Thor's Hammer", price: 400, quantity: 10},
+  {id: 2, item: "Iron Man's Suit", price: 200, quantity: 310},
+  {id: 3, item: "Cap's Shield", price: 50, quantity: 5},
+  {id: 4, item: "Black Widow's Gauntlets", price: 100, quantity: 150},
+  {id: 5, item: "Hawkeye's Bow", price: 300, quantity: 0}
+ ]
+ 
+ function discountInventory(arr) {
+  const discount = 0.10;
+
+  return arr.map((item) => {
+    return {
+      id: item.id,
+      item: item.item,
+      price: item.price,
+      quantity: item.quantity,
+      discount: discount,
+      "discounted price": item.price - (item.price * discount)
+    } 
+  })
+ }
+
+ console.log(discountInventory(inventory))
+
+// // notice how we can use forEach for the exact same process
+// // that said, we'd have to init the array then push into it
+// // map creates an array and places the returned array items to this new array 
+  // arr.forEach((item) => {
+
+  // let newInventory = [];
+
+  // newInventory.push({
+  //   id: item.id,
+  //   item: item.item,
+  //   price: item.price,
+  //   quantity: item.quantity,
+  //   discount: discount,
+  //   "discounted price": item.price - (item.price * discount)
+  // })
+  //   })
