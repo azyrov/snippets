8b1dddd6fcb9773ae009e2fc558fe034f2acaf6c
azyrov <for28@yahoo.com>
2026-09-22 05:23:46 +0000

    Add basic setup for dataset files
    This is a double commit from previous commits but those were used to commit staging after removing index.html and index.js from gitignore

--- data-attributes/index.js
+++ data-attributes/index.js
@@ -3,10 +3,11 @@ let parent = document.querySelector("#parent");
 
 
 parent.addEventListener("click", (e) => {
+
     const value = e.target.dataset.value;
     console.log(value);
 
-    if (value === "child-one") {
+    if (value === `child-one`) {
         console.log(`I am ${value}`)
     } else if (value === `child-two`) {
         console.log(`I am ${value}`)
