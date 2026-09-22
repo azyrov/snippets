6e1b933d5d6966caf328547e0bac87d6e9826005
azyrov <for28@yahoo.com>
2026-09-22 05:47:40 +0000

    Add closest method

--- data-attributes/index.js
+++ data-attributes/index.js
@@ -3,9 +3,21 @@ let parent = document.querySelector("#parent");
 
 
 parent.addEventListener("click", (e) => {
+    /* WHY WE NEED closest() */
     // clicking anywhere within the parent will still trigger the parent though
     // try clicking in the gaps to prove this
-    const value = e.target.dataset.value;
+    // these log `undefined` because these clicked locations have no dtaset value to produce
+    // of course, we can give the parent a data-value in html so that is triggered
+
+    /* USING closest() */
+    // we store the closest ancestor to the target with the button element
+    const button = e.target.closest("button");
+
+    // if the triggered location does not have an ancester "button" element, do nothing and return
+    // in other words, do nothing 
+    if (!(button)) return;
+
+    const value = button.dataset.value;
     console.log(value);
 
     if (value === `child-one`) {
